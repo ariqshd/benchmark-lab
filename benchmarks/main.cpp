6961b7adee7b88465a031c9c35e038226ea9ea43
ariq <t104218@singaporetech.edu.sg>
2026-04-23 17:39:06 +0800

    test read vector & linkedlist

--- benchmarks/main.cpp
+++ benchmarks/main.cpp
@@ -1,5 +1,6 @@
 #include <benchmark/benchmark.h>
 #include <bl/vector.h>
+#include <forward_list>
 
 // Benchmark 1: Standard Vector Copy
 // Measures allocation + copy cost
@@ -54,6 +55,78 @@ static void BM_MathAdd(benchmark::State& state) {
   }
 }
 BENCHMARK(BM_MathAdd);
+
+// The number of items we will insert
+const int ITEM_COUNT = 10000;
+
+static void BM_VectorInsertFront(benchmark::State& state){
+    for(auto _ : state) {
+        std::vector<int> v;
+
+        for(int i = 0; i < ITEM_COUNT; ++i){
+            // O(n) insertion - shifting elements
+            v.insert(v.begin(), i);
+        }
+
+        // Prevent DCE
+        benchmark::DoNotOptimize(v.data());
+    }
+}
+BENCHMARK(BM_VectorInsertFront);
+
+static void BM_ListInsertFront(benchmark::State& state) {
+  for (auto _ : state) {
+    std::forward_list<int> l;
+
+    for (int i = 0; i < ITEM_COUNT; ++i) {
+      // O(1) insertion - pointer update
+      l.push_front(i);
+    }
+
+    // Prevent DCE - forward_list doesn't have .data()
+    // so we use the front element
+    benchmark::DoNotOptimize(l.front());
+  }
+}
+BENCHMARK(BM_ListInsertFront);
+
+static void BM_VectorTraverse(benchmark::State& state) {
+  // Setup: Create a large vector
+  std::vector<int> v(ITEM_COUNT);
+  std::fill(v.begin(), v.end(), 1);
+
+  for (auto _ : state) {
+    long long sum = 0;
+
+    // The Algorithm: Linear Scan
+    for (int i : v) {
+      sum += i;
+    }
+
+    benchmark::DoNotOptimize(sum);
+  }
+}
+BENCHMARK(BM_VectorTraverse);
+
+static void BM_ListTraverse(benchmark::State& state) {
+  // Setup: Create a large list
+  std::forward_list<int> l(ITEM_COUNT);
+  std::fill(l.begin(), l.end(), 1);
+
+  for (auto _ : state) {
+    long long sum = 0;
+
+    // The Algorithm: Linear Scan
+    for (int i : l) {
+      sum += i;
+    }
+
+    benchmark::DoNotOptimize(sum);
+  }
+}
+BENCHMARK(BM_ListTraverse);
+
+
 
 // Since we linked benchmark::benchmark_main, we don't need
 // to write our own main() function.
